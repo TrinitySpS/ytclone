481302d2dd2c6acc7b90485823a677053f773815
trinity-senpai <45470217+trinitysenpai@users.noreply.github.com>
2020-10-23 12:18:35 -0900

    Add files via upload

--- public/js/js.js
+++ public/js/js.js
@@ -4,44 +4,42 @@
 // 	window.location.reload()
 // 	break		
 // 	}
-// } 
+// }
+notify = confirm('This is only fit for mobile devices');
 
-while (screen.width > 700){
-	new jBox('Modal', {
-		attach: '.thumbnail',
-		title: 'Device not supported',
-		content: 'your device is not supported',
-		color: 'red'
-	})}//close showaccount function
-	
 $(function(){
 	//MOVING TO OTHER TABS
 	$(".sub-container").hide();	
+	$(".settings-container").hide();	
+
 	//HOME TAB
-	$(".material-icons").eq(1).click(function(){
-		$(".indicator").css("margin-left","16%");//the tab white indicator
+	$("i").eq(1).click(function(){
+		$(".indicator").css("margin-left","17%");//the tab white indicator
 		$('.material-icons').eq(0).css("margin-left","50vw");//aligning the theme button
 
 		$(".container").show();//show the home container
 		$(".home").html("Home");// change the title
+		$(".settings-container").hide();//hide settings page
 		$(".sub-container").hide();	//hide subscriptions content
 	})
 	
 	//SUBS TAB
-	$(".material-icons").eq(2).click(function(){
-		$(".indicator").css("margin-left","43vw");
+	$("i").eq(2).click(function(){
+		$(".indicator").css("margin-left","44vw");
 		$('.material-icons').eq(0).css("margin-left","26vw");// the theme icon button
 		$(".container").hide();
+		$(".settings-container").hide();
 		$(".home").html("Recommended");
 		$(".sub-container").show();
 	})
 	
 	//SETTINGS AND ACCOUNT TAB
-	$(".material-icons").eq(3).click(function(){
-		$(".indicator").css("margin-left","70vw");
+	$("i").eq(3).click(function(){
+		$(".indicator").css("margin-left","71vw");
 		$('.material-icons').eq(0).css("margin-left","52vw");// the theme icon button
 		$(".container").hide();
 		$(".sub-container").hide();
+		$(".settings-container").show();
 		$(".home").html("Tools");
 
 	})
@@ -50,20 +48,20 @@ $(function(){
 	let theme;
 	theme = themes[0];
 	//DARK THEME FEATURE
-	$('.material-icons').eq(0).click(function(){
+	$('i').eq(0).click(function(){
 		$("body").toggleClass('dark');//switches to dark
 		$("body").toggleClass('light');//switches back
 		
 		if (theme == 2)
 		{
-			$('.material-icons').css('color','black');
+			$('i').css('color','black');
 		}
 		else if (theme == 1){
-			$('.material-icons').css('color','white');
+			$('i').css('color','white');
 
 		}
 		console.log(theme)
-		$('.material-icons').toggleClass('dark-icon');
+		$('i').toggleClass('dark-icon');
 
 		$('.avatar').toggleClass('dark-border')
 		$('.thumbnail').css('box-shadow','none');
